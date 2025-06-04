0d36662a21d6f0bd7648d55819709cbd86a86187
Eder-afk <ezapata.a03@gmail.com>
2025-06-04 14:49:39 -0500

    refactor: add Dependency Inversion to RepositorioProyectos

--- Portafolio/Services/RepositorioProyectos.cs
+++ Portafolio/Services/RepositorioProyectos.cs
@@ -2,7 +2,12 @@
 
 namespace Portafolio.Services
 {
-    public class RepositorioProyectos
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+
+    public class RepositorioProyectos : IRepositorioProyectos
     {
         public List<Proyecto> ObtenerProyectos()
         {
